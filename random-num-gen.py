2c84a51e88454f6c641a8694dca5f9343f7cb9d0
Rebekah <macrosystem@proton.me>
2023-05-07 15:25:36 +0200

    Update random-num-gen.py

--- random-num-gen.py
+++ random-num-gen.py
@@ -1,1 +1,12 @@
+import datetime #lets try later with zero imports.
+
 #tryting to make a random num gen without a pre-made random lib
+
+def rand_date_time():
+    now = datetime.datetime.now()
+    date_int = int(now.strftime('%d%m%y'))
+    time_int = int(now.strftime('%H%M'))
+    var_name = date_int / time_int 
+    return var_name[0]
+  
+print(rand_date_time())
