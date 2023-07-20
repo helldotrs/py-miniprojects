523cf884796d28404432187f5f7c8ed0e6270fdb
Rebekah <macrosystem@proton.me>
2023-07-20 23:13:49 +0200

    Update word-avg-len.py

--- word-avg-len.py
+++ word-avg-len.py
@@ -1,6 +1,7 @@
+import re
 
 def avg_len(in_str):
-  number_of_words        = len(in_str.split)
+  number_of_words        = len(in_str.split())
   number_of_alpha_chars  = len(re.sub(r'[^a-zA-Z]', '', in_str))
   return number_of_alpha_chars / number_of_words
 
