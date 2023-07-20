219baacc17640b72365b2a3f5e56b9d801a622a0
Rebekah <macrosystem@proton.me>
2023-07-20 23:09:27 +0200

    Update word-avg-len.py

--- word-avg-len.py
+++ word-avg-len.py
@@ -1,5 +1,7 @@
 
 def avg_len(in_str):
-  number_of_words = len(in_str.split)
+  number_of_words        = len(in_str.split)
+  number_of_alpha_chars  = len(re.sub(r'[^a-zA-Z]', '', input_string))
+  return number_of_alpha_chars / number_of_words
 
 print(avg_len("this is a string with some words in it")
