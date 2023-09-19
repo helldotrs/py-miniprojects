1ea8122c89d1118d5238bca20a307aae42eb5fdb
Rebekah <macrosystem@proton.me>
2023-09-19 18:17:52 +0200

    Update and rename 6d-dice-tester.py to dice-tester-d6.py

---
+++ dice-tester-d6.py
@@ -0,0 +1,10 @@
+test_string = "6364215362142415442661153362442362523636126431443112332443614254455251415432511525231"
+d = 6 //number of sides, max 9
+
+for a in test_string:
+  if(a.isdigit() and 1 <= int(a) <= d):
+    filtered_test_string
+
+test_string = filtered_test_string
+
+
