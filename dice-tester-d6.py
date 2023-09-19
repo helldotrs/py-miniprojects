108555371bfe70f94683f483a05b1ed536c264da
Rebekah <macrosystem@proton.me>
2023-09-19 18:32:14 +0200

    Update dice-tester-d6.py

--- dice-tester-d6.py
+++ dice-tester-d6.py
@@ -1,5 +1,6 @@
-test_string = "6364215362142415442661153362442362523636126431443112332443614254455251415432511525231"
-d = 6 //number of sides, max 9
+test_string  = "6364215362142415442661153362442362523636126431443112332443614254455251415432511525231"
+d            = 6 #number of sides, only currently works for 6
+output       = [0,0,0, 0,0,0]
 
 for a in test_string:
   if(a.isdigit() and 1 <= int(a) <= d):
@@ -7,4 +8,7 @@ for a in test_string:
 
 test_string = filtered_test_string
 
+for a in test_string:
+  output[int(a)-1] += 1
 
+print(output)
