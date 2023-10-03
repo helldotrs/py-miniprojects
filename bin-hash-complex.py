719b4dd820ca4e1894e84d9bb68aac0b0d74ba17
Rebekah <macrosystem@proton.me>
2023-10-03 20:31:24 +0200

    Update bin-hash-complex.py

--- bin-hash-complex.py
+++ bin-hash-complex.py
@@ -50,6 +50,9 @@ def print_hash(a):
     print(hasher(a))
 
 print_hash("a")
+print_hash("b")
+print_hash("c")
 print_hash("foo bar")
 print_hash("password123")
 print_hash("abcefghijklmnopqrstuvwxyz")
+print_hash("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
