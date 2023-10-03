679c8b7990628ed24c7131e792944bba9e76ff64
Rebekah <macrosystem@proton.me>
2023-10-03 19:52:47 +0200

    Update bin-hash.py

--- bin-hash.py
+++ bin-hash.py
@@ -1,1 +1,13 @@
 input = "foobar"
+
+def convert_to_binary(string):
+    binary_result = ""
+    for char in string:
+        ascii_code = ord(char)  # Get the ASCII code of the character
+        if ascii_code % 2 == 0: 
+            binary_result += '0' 
+        else:
+            binary_result += '1'  # If odd, append '1' to the result string
+    return binary_result
+
+
