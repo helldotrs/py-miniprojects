7402c582ec89352bccb58cde38af403180f80cff
Rebekah <macrosystem@proton.me>
2023-10-06 23:32:42 +0200

    Update problem-solving-2023-10-06.py

--- problem-solving-2023-10-06.py
+++ problem-solving-2023-10-06.py
@@ -16,3 +16,21 @@ def print_count(a):
     print(count_words(a))
 
 print_count("Hello, world! This is a beautiful world.")
+
+
+#chatGPTs version:
+"""
+import re
+from collections import Counter
+
+def count_words(sentence):
+    cleaned_words = re.findall(r'\w+', sentence.lower())  # extract words using regex and convert to lowercase
+    word_count = Counter(cleaned_words)  # count word frequencies using Counter
+    return word_count
+
+def print_count(sentence):
+    word_count = count_words(sentence)
+    print(word_count)
+
+print_count("Hello, world! This is a beautiful world.")
+"""
