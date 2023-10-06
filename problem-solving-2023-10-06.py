3e7354416006a2018db9412648c03cb27311643b
Rebekah <macrosystem@proton.me>
2023-10-06 22:37:53 +0200

    Create problem-solving-2023-10-06.py

---
+++ problem-solving-2023-10-06.py
@@ -0,0 +1,2 @@
+#Task: write a Python program that takes a sentence as input and counts the frequency of each word in the sentence. The program should ignore punctuation and consider words case-insensitively. Output the word frequencies as a dictionary where keys are the words and values are the corresponding frequencies.
+
