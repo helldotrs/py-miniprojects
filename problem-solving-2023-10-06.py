a3f9e840a75de5ed9e8fa82fbf9cfc0f48e7439f
Rebekah <macrosystem@proton.me>
2023-10-06 23:09:06 +0200

    Update problem-solving-2023-10-06.py

--- problem-solving-2023-10-06.py
+++ problem-solving-2023-10-06.py
@@ -1,6 +1,9 @@
-#Task: write a Python program that takes a sentence as input and counts the frequency of each word in the sentence. The program should ignore punctuation and consider words case-insensitively. Output the word frequencies as a dictionary where keys are the words and values are the corresponding frequencies.
+#Task: write a Python program that takes a sentence as input and counts the frequency of each word in the sentence. The program should ignore punctuation and consider words case-insensitively. Output the word frequencies as a dictionary where keys are the words and values are the corresponding frequencies
 
-input_string = "Hello, world! This is a beautiful world."
+input_string  = "Hello, world! This is a beautiful world."
 
-output_list =  []
+input_string = input_string.lower()
+print (input_string)
 
+word_list = [word in input_string.split()]
+print (word_list)
