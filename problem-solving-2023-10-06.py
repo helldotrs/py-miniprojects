6e52ba6f28d9d8dc03f4567ba258e0b128ab9362
Rebekah <macrosystem@proton.me>
2023-10-06 23:18:15 +0200

    Update problem-solving-2023-10-06.py

--- problem-solving-2023-10-06.py
+++ problem-solving-2023-10-06.py
@@ -1,6 +1,13 @@
 #Task: write a Python program that takes a sentence as input and counts the frequency of each word in the sentence. The program should ignore punctuation and consider words case-insensitively. Output the word frequencies as a dictionary where keys are the words and values are the corresponding frequencies
 
+import re
+
+def remove_punctuation(a):
+    return re.sub(r"^\w\s", "", a)
+
 input_string  = "Hello, world! This is a beautiful world."
+
+input_string = remove_punctuation(input_string)
 
 input_string = input_string.lower()
 print (input_string)
