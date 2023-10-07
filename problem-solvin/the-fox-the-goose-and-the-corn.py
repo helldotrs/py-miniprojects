9e0e2494e7a0b78ca37ea893bdc522fe5f0f003d
Rebekah <macrosystem@proton.me>
2023-10-07 20:11:56 +0200

    Update the-fox-the-goose-and-the-corn.py

--- problem-solvin/the-fox-the-goose-and-the-corn.py
+++ problem-solvin/the-fox-the-goose-and-the-corn.py
@@ -1,4 +1,14 @@
-#If the fox is left alone with the goose it will eat the goose, if the goose is left alone with the corn it will eat corn, the boat only has room for the farmer and one more item/animal. how can the farmer cross the river? 
+#If the fox is left alone with the goose it will eat the goose, 
+#if the goose is left alone with the corn it will eat corn, 
+#the boat only has room for the farmer and one more item/animal. 
+#how can the farmer cross the river? 
 
 north_side = []
-south_side = ["farmer", "fox", "goose", "corne"]
+south_side = ["farmer", "fox", "goose", "corner"]
+
+def n2s(a):
+  north_side.remove(a)
+  north_side.remove("farmer")
+
+  south_side.append(a)
+  south_side.append("farmer")
