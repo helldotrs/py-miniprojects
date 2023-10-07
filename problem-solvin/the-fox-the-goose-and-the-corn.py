58ce5d4ced979ca0a303ca72ac483f96d99cef2b
Rebekah <macrosystem@proton.me>
2023-10-07 20:03:20 +0200

    Create the-fox-the-goose-and-the-corn.py

---
+++ problem-solvin/the-fox-the-goose-and-the-corn.py
@@ -0,0 +1,4 @@
+#If the fox is left alone with the goose it will eat the goose, if the goose is left alone with the corn it will eat corn, the boat only has room for the farmer and one more item/animal. how can the farmer cross the river? 
+
+north_side = []
+south_side = ["farmer", "fox", "goose", "corne"]
