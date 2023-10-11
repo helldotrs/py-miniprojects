bd6cd7a19c7c4dc199f25bc383919f8b17a9ce31
Rebekah <macrosystem@proton.me>
2023-10-12 00:40:43 +0200

    add slider_col

--- problem-solving/quarrasi-problem-manual.py
+++ problem-solving/quarrasi-problem-manual.py
@@ -1,6 +1,7 @@
 slider_0    = [True,  False, True,   True   ]
 slider_1    = [True,  True,  False,  False  ]
 slider_2    = [False, True,  False,  False  ]
+slider_col  = [slider_0, slider_1, slider_2 ]
 slider_pos  = ["right",  "right",  "right"  ] # val --> left/right
 
 cols = []
