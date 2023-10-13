2194cbdc6ff9a64263614f0e388ecb9b4c1a7d9d
Rebekah <macrosystem@proton.me>
2023-10-13 12:35:10 +0200

    Update quarrasi-problem-tester.py

--- problem-solving/quarrasi-problem-tester.py
+++ problem-solving/quarrasi-problem-tester.py
@@ -8,6 +8,8 @@ slider_pos  = ["right",  "right",  "right"  ] # val --> left/right
 
 def calculate_col_values(side):
     for a in slider_pos if slider_pos == side:
-        pass
+        print("y") # test
+        
     # check slide side. for now assume all are on the right side
     
+calculate_col_value("right")
