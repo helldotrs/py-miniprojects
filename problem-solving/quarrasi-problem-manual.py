52ef5230d80597780cb025aef4914908c4536d4a
Rebekah <macrosystem@proton.me>
2023-10-13 12:18:04 +0200

    Update quarrasi-problem-manual.py

--- problem-solving/quarrasi-problem-manual.py
+++ problem-solving/quarrasi-problem-manual.py
@@ -4,16 +4,13 @@ slider_1    = [True,  True,  False,  False  ]
 slider_2    = [False, True,  False,  False  ]
 """
 sliders     = [
-  [ 
-  ]
-  
-  [
     [True,  False, True,   True   ]
     [True,  True,  False,  False  ]
     [False, True,  False,  False  ]
-  ]
 ]
 slider_col  = [slider_0, slider_1, slider_2 ]
 slider_pos  = ["right",  "right",  "right"  ] # val --> left/right
+
+
 
 cols = []
