1b1ecfbb9004fb0cbfae7d0acb701ccc5731af4b
Rebekah <macrosystem@proton.me>
2023-10-14 17:20:11 +0200

    implimenting lm fix
    checked - makes sense and works.

--- problem-solving/quarrasi-problem-tester.py
+++ problem-solving/quarrasi-problem-tester.py
@@ -1,18 +1,23 @@
-SLIDERS    = [
-    [True,  False, True,   True   ]
-    [True,  True,  False,  False  ]
-    [False, True,  False,  False  ]
+SLIDERS = [
+    [True, False, True, True],
+    [True, True, False, False],
+    [False, True, False, False]
 ]
-slider_pos  = ["right",  "right",  "right"  ] # val --> left/right
+
+slider_pos = ["right", "right", "right"]  # val --> left/right
 
 def calculate_col_totals(side):
-    col_totals = []
-    for a in slider_pos:
-        col_totals[a][for b in range SLIDERS[0][0]]
+    col_totals = [0] * len(SLIDERS[0])  # Initialize col_totals list with zeros
+
+    # Iterate through sliders and update col_totals based on their positions
+    for i in range(len(SLIDERS)):
+        for j in range(len(SLIDERS[i])):
+            if slider_pos[i] == side and SLIDERS[i][j]:
+                col_totals[j] += 1
 
     return col_totals
-        
-        
-    # check slide side. for now assume all are on the right side
-    
-calculate_col_value("right")
+
+# Check slide side (for now assume all are on the right side)
+side = "right"
+col_totals = calculate_col_totals(side)
+print(f"Column totals for sliders on the {side} side: {col_totals}")
