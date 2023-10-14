08a42139781cc1e8bcf82a2ce429827896bf2fb4
Rebekah <macrosystem@proton.me>
2023-10-14 17:55:12 +0200

    minor bug fix

--- problem-solving/quarrasi-problem-tester.py
+++ problem-solving/quarrasi-problem-tester.py
@@ -25,7 +25,7 @@ def test_alarm(side): # could probably be split into two functions
 def test_alarms(left,right): # is this name unique enough of could it be something like test_both_alarms()? or should I just stack??
     return test_alarm(left) or test_alarm(right) # FIXME: assuming non-exclusivity here. look up rules.
 
-def main()
+def main():
     pass
 
 cols_right   = calculate_col_totals("right")
