8f5136d02858298f5cb284ff5ff162badbd9f903
Rebekah <macrosystem@proton.me>
2024-04-05 20:53:03 +0200

    Update reverse-string.py

--- reverse-string.py
+++ reverse-string.py
@@ -1,4 +1,9 @@
-my_string = "thesearecommonwords"
+reverse(str_in):
+    str_out = ""
+    for char in str_in:
+        str_out = char + str_out
+    return str_out
 
-reverse(a):
-    pass
+my_string = input("reverse:")
+
+print(reverse(my_string))
