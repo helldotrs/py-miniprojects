2038b3537081292a2987d365d7e89cfac33d7e50
Rebekah <macrosystem@proton.me>
2024-04-22 21:33:59 +0200

    Create scope.py

---
+++ experiments/scope.py
@@ -0,0 +1,13 @@
+my_var = None  
+if this == "that":
+  my_var = "text"
+  print(my_var)
+print(my_var)  
+
+this = "that"
+
+my_var = None  
+if this == "that":
+  my_var = "text"
+  print(my_var)
+print(my_var)  
