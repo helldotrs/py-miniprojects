274f564b8ffec9782ec751dd5e32cc67da9ee2aa
Rebekah <macrosystem@proton.me>
2024-05-09 23:24:16 +0200

    Update scope.py

--- experiments/scope.py
+++ experiments/scope.py
@@ -1,13 +1,13 @@
 my_var = None  
-if this == "that":
-  my_var = "text"
+if this == "that 1":
+  my_var = "text 2"
   print(my_var)
 print(my_var)  
 
-this = "that"
+this = "that 3"
 
 my_var = None  
-if this == "that":
-  my_var = "text"
+if this == "that 4":
+  my_var = "text 5"
   print(my_var)
 print(my_var)  
